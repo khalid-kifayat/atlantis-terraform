363f464020d1f2a689e95bfa6e224d4d3bea48ab
khalid-kifayat <khalid.kifayat@gmail.com>
2022-12-20 11:51:41 +0500

    added

---
+++ server/middleware.go
@@ -0,0 +1,38 @@
+// Copyright 2017 HootSuite Media Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the License);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an AS IS BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// Modified hereafter by contributors to runatlantis/atlantis.
+
+package server
+
+import (
+	"net/http"
+
+	"github.com/runatlantis/atlantis/server/logging"
+	"github.com/urfave/negroni"
+)
+
+// NewRequestLogger creates a RequestLogger.
+func NewRequestLogger(logger logging.SimpleLogging) *RequestLogger {
+	return &RequestLogger{logger}
+}
+
+// RequestLogger logs requests and their response codes.
+type RequestLogger struct {
+	logger logging.SimpleLogging
+}
+
+// ServeHTTP implements the middleware function. It logs all requests at DEBUG level.
+func (l *RequestLogger) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	l.logger.Debug("%s %s – from %s", r.Method, r.URL.RequestURI(), r.RemoteAddr)
+	next(rw, r)
+	l.logger.Debug("%s %s – respond HTTP %d", r.Method, r.URL.RequestURI(), rw.(negroni.ResponseWriter).Status())
+}
